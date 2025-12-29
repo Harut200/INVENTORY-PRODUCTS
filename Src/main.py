b3c9c88bb6575b77e1e6b8e02b45cef56083a55f
Harut <harut@Haruts-MacBook-Air.local>
2025-12-29 20:40:28 +0400

    First commit

---
+++ Src/main.py
@@ -0,0 +1,122 @@
+import json
+import sqlite3
+import requests
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+DB_PATH = Path("../Data/DB/EXAM.db")
+
+prod_raw = pd.read_csv(
+    Path.cwd().parent / "Data" / "Row" / "products_noisy.csv"
+)
+
+
+prod_raw["ProductID"] = range(101, len(prod_raw) + 101)
+
+prod_raw["ProductName"] = [f"Product_{i}" for i in range(1, 1001)]
+
+prod_raw["Category"] = prod_raw["Category"].fillna(prod_raw["Category"].mode()[0])
+
+prod_raw["Price"] = prod_raw["Price"].mask(prod_raw["Price"] <= 0, prod_raw["Price"].median()).fillna(prod_raw["Price"].median())
+
+
+
+
+inv_raw = pd.read_csv(
+    Path.cwd().parent / "Data" / "Row" / "inventory_noisy.csv"
+)
+inv_raw
+
+inv_raw["ProductID"] = range(101, len(inv_raw) + 101)
+
+inv_raw["InventoryID"] = range(1, len(inv_raw) + 1)
+
+inv_raw["WarehouseCode"] = inv_raw["WarehouseCode"].fillna(inv_raw["WarehouseCode"].mode()[0])
+
+inv_raw["StockLevel"] = inv_raw["StockLevel"].fillna(inv_raw["StockLevel"].median())
+
+inv_raw.to_csv(Path.cwd().parent / "Data" / "Processed" / "investory_levels.csv")
+
+prod_raw.to_csv(Path.cwd().parent / "Data" / "Processed" / "products.csv")
+
+
+
+conn = sqlite3.connect(DB_PATH)
+cur = conn.cursor()
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS products(
+    ProductID INTEGER PRIMARY KEY, 
+    ProductName TEXT,
+    Category TEXT,
+    Price REAL
+    );
+    """
+)
+
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS investory(
+    InventoryID INTEGER PRIMARY KEY,
+    ProductID INTEGER REFERENCES products (ProductID),
+    WarehouseCode TEXT,
+    StockLevel INTEGER
+    );
+    """
+)
+
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS sales(
+    SaleID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ProductID INTEGER REFERENCES products (ProductID),
+    QuantitySold INTEGER,
+    SaleDate TEXT DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+)
+
+conn.commit()
+
+prod_raw.to_sql("products", conn, if_exists="replace", index=False)
+inv_raw.to_sql("investory", conn, if_exists="replace", index=False)
+
+cur.execute(
+    """
+    CREATE TRIGGER IF NOT EXISTS UpdateStockAfterSale
+    AFTER INSERT ON sales
+    BEGIN
+        UPDATE Inventory
+        SET StockLevel = StockLevel - NEW.QuantitySold
+        WHERE ProductID = NEW.ProductID;
+    END;
+    """
+)
+
+cur.execute("""
+    CREATE VIEW IF NOT EXISTS CategoryRevenueSummary AS
+    SELECT
+        p.Category,
+        SUM(p.Price * i.StockLevel) AS TotalPotentialRevenue
+    FROM products p
+    JOIN inventory i
+    ON p.ProductID = i.ProductID
+    GROUP BY p.Category;
+    """
+)
+
+cur.execute("""
+    UPDATE products
+    SET Price = Price * 0.8
+    WHERE ProductID IN (
+        SELECT ProductID
+        FROM investory
+        WHERE WarehouseCode = 'WH-A'
+          AND StockLevel < 40
+    );
+    """
+)
+
+conn.commit()
+conn.close()
